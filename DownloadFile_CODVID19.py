5123e546051a7c4a4a236a51cc7fcad2e92fa4f1
Manuel Lamarche <mlamarche08@gmail.com>
2020-03-26 10:13:03 -0400

    Modificacion en el formato de la fecha

--- DownloadFile_CODVID19.py
+++ DownloadFile_CODVID19.py
@@ -1,9 +1,10 @@
-from datetime import date
+from datetime import datetime
 import requests
 
+now = datetime.now() # current date and time
 
 def download():
-    filename = today.strftime("%m-%d-%Y") + ".csv" 
+    filename = now.strftime("%m-%d-%Y")+ ".csv" 
     url = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/" + filename
     
     if requests.status_codes == 200:
@@ -17,7 +18,7 @@ def download():
 
 
 def OlderFiles():
-    for filename in ["03-21-2020.csv","03-22-2020.csv","03-23-2020.csv","03-24-2020.csv"]:
+    for filename in ["03-21-2020.csv","03-22-2020.csv","03-23-2020.csv","03-24-2020.csv","03-25-2020.csv","03-26-2020.csv"]:
             url = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/" + filename
             request = requests.get(url)
             if request.status_code == 200:
@@ -28,10 +29,10 @@ def OlderFiles():
             else:
                 print("File not found - 404 : " + filename)
             
-OlderFiles()           
+# OlderFiles()           
 
 
-
+download()
 
 
        
